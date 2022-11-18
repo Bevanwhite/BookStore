998d317f0bed04350f26bf28ec3b1594e50f71ad
Jeffery White <jefferybevan@gmail.com>
2022-11-19 00:21:48 +0530

    added mvc function to asp.net application

---
+++ Beva.BookStore/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Beva.BookStore.Controllers
+{
+	public class HomeController : Controller
+	{
+		public string Index()
+		{
+			return "beva Origami";
+		}
+	}
+}
